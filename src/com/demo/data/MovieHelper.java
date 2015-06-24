383ff03fee5d8f6fde619aa9b575f59f5f112a06
wk476855 <wk476855@126.com>
2015-06-24 10:53:50 +0800

    data helper

---
+++ src/com/demo/data/MovieHelper.java
@@ -0,0 +1,13 @@
+package com.demo.data;
+
+import com.demo.resource.HotMovie;
+
+/**
+ * Created by wk on 2015/6/24.
+ */
+public class MovieHelper {
+
+	public HotMovie getHotMovie(String city) {
+		return new HotMovie();
+	}
+}
---
+++ src/com/demo/data/TheaterHelper.java
@@ -0,0 +1,20 @@
+package com.demo.data;
+
+import com.demo.resource.Theater;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wk on 2015/6/24.
+ */
+public class TheaterHelper {
+
+	public List<Theater> getTheaterNearby(double lat, double lng) {
+		return new ArrayList<Theater>();
+	}
+
+	public Theater getTheaterDetails(String uid) {
+		return new Theater();
+	}
+}
